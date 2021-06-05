1915204dfa92944a3c7877712d5aa9a173788637
Engel <1283164060121002@mil>
2021-06-05 00:17:26 -0400

    first commit

--- AKJams.py
+++ AKJams.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import chardet
 #import numpy as np
 import pandas as pd
 import altair as alt
@@ -13,7 +14,10 @@ st.set_page_config(layout="wide")
 
 #@st.cache(persist=True)
 def get_ice_jam_csv(file_name):
-    df = pd.read_csv(file_name)
+    with open(filename, 'rb') as f:
+        result = chardet.detect(f.read())
+    
+    df = pd.read_csv(file_name,encoding=result['encoding'])
     df = df.rename(columns={"Latitude" : "lat", "Longitude" : "lon", "Water year" : "WY"})
     df.lon = pd.to_numeric(df.lon,errors="coerce")
     df.lat = pd.to_numeric(df.lat,errors="coerce")
